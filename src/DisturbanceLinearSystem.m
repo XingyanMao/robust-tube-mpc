003fc6438ff70741250b0bdb48f34fb1fad50463
XY_Mao <50959195+XingyanMao@users.noreply.github.com>
2022-03-15 20:41:12 +0800

    Update DisturbanceLinearSystem.m
    about the quick calculation of mRPI set.

--- src/DisturbanceLinearSystem.m
+++ src/DisturbanceLinearSystem.m
@@ -9,9 +9,9 @@ classdef DisturbanceLinearSystem < LinearSystem
 
         function obj = DisturbanceLinearSystem(A, B, Q, R, W)
             obj = obj@LinearSystem(A, B, Q, R);
-
             obj.W = W;
-            obj.Z = obj.compute_mrpi_set(1e-4);
+            Nmax=25;
+            obj.Z = obj.compute_mrpi_set(Nmax);
         end
 
         function x_new = propagate(obj, x, u)
@@ -28,46 +28,52 @@ classdef DisturbanceLinearSystem < LinearSystem
             verts = obj.W.V;
             b_max = max(verts)';
             b_min = min(verts)';
-
+            w = rand(obj.nx, 1) .* (b_max - b_min) + b_min; 
             % generate random until it will be inside of W
-            while true
-                w = rand(obj.nx, 1) .* (b_max - b_min) + b_min; 
-                if obj.W.contains(w)
-                    break
-                end
-            end
+%             while true
+%                 w = rand(obj.nx, 1) .* (b_max - b_min) + b_min; 
+%                 if obj.W.contains(w)
+%                     break
+%                 end
+%             end
         end
 
-        function Fs = compute_mrpi_set(obj, epsilon)
-            % Computes an invariant approximation of the minimal robust positively
-            % invariant set for 
-            % x^{+} = Ax + w with w \in W
-            % according to Algorithm 1 in 'Invariant approximations of
-            % the minimal robust positively invariant set' by Rakovic et al. 
-            % Requires a matrix A, a Polytope W, and a tolerance 'epsilon'.  
-            [nx,~] = size(obj.Ak); 
-            s = 0; 
-            alpha = 1000;
-            Ms = 1000;
-            E = eye(nx);
-            it = 0;
-            while(alpha > epsilon/(epsilon + Ms))
-                s = s+1;
-                alpha = max(obj.W.support(obj.Ak^s*(obj.W.A)')./obj.W.b);
-                mss = zeros(2*nx,1);
-                for i = 1:s
-                    mss = mss+obj.W.support([obj.Ak^i, -obj.Ak^i]);
+        function Fs = compute_mrpi_set(obj, Nmax)
+%             % Computes an invariant approximation of the minimal robust positively
+%             % invariant set for 
+%             % x^{+} = Ax + w with w \in W
+%             % according to Algorithm 1 in 'Invariant approximations of
+%             % the minimal robust positively invariant set' by Rakovic et al. 
+%             % Requires a matrix A, a Polytope W, and a tolerance 'epsilon'. 
+            AkW = cell(Nmax + 1, 1);
+            alphaW = cell(Nmax + 1, 1);
+            alphas = NaN(Nmax + 1, 1);
+            Ak=eye(2);
+            verts = (obj.W.V)';
+            for n = 1:(Nmax + 1)
+                AkW{n} = Ak*verts;
+                normW = norm(verts,inf);
+                normKW=norm(obj.K*verts,inf);
+                normAkW = norm(AkW{n},inf);
+                normKAkW = norm(obj.K*AkW{n},inf);
+                
+                alphas(n) = max(normAkW/normW, normKAkW/normKW);
+                alphaW{n} = alphas(n)*verts;
+                Ak=obj.Ak*Ak;
+                PAkw{n}=Polyhedron((AkW{n})');  
+                if n>5
+                    if max(max(abs(AkW{n}-AkW{n-1})))<0.001
+                        break
+                    end    
                 end
-                Ms = max(mss);
-                it = it+1;
             end
-
-            Fs = obj.W;
-            for i =1:s-1
-                Fs = Fs+obj.Ak^i*obj.W;
-            end
-            Fs = (1/(1-alpha))*Fs;
-        end
+            
+            Fs=PAkw{1};
+            for i=2:n
+                Fs=Fs+PAkw{i};
+            end 
+            Fs=Fs*(1/(1-alphas(n)));
+         end
 
     end
 end
